e7142db2276fd0ef88e7baf72ac41af05c5a0989
Samuel Belmont <91174995+SamBMz@users.noreply.github.com>
2025-02-26 00:49:31 -0600

    Update cli.py
    Reestructura de la función confront() debido al descarte de la modificación de la función IA pasada por parámetros en el archivo principal

--- components/cli.py
+++ components/cli.py
@@ -1,11 +1,14 @@
 import os
 from components.IA_machine import *
-#Necesario para retornar las funciones de comportamiento de IA
+
+
+# Necesario para retornar las funciones de comportamiento de IA
 
 def clean_screen():
     """Limpia la terminal."""
     os.system('cls' if os.name == 'nt' else 'clear')
-    
+
+
 def symbol():
     print("¿Qué marca escoges? 'X' o 'O'")
     while True:
@@ -23,7 +26,8 @@ def symbol():
         else:
             clean_screen()
             print("Ingresa una letra válida: X o O")
-            
+
+
 def selection():
     """Inicia el juego del gato."""
     print("\tESTE ES EL JUEGO DEL GATO. ¿QUIERES TIRAR PRIMERO O LA MÁQUINA?")
@@ -33,10 +37,10 @@ def selection():
             shoter = int(input("1) Usuario tira primero\n2) Máquina tira primero\n"))
             if shoter == 1:
                 clean_screen()
-                return 1 # Devuelve la elección del jugador
+                return 1  # Devuelve la elección del jugador
             elif shoter == 2:
                 clean_screen()
-                return 2 # Devuelve la elección de la máquina
+                return 2  # Devuelve la elección de la máquina
             else:
                 clean_screen()
                 print("Ingrese un valor válido (1 o 2).")
@@ -45,26 +49,15 @@ def selection():
             clean_screen()
             print("Entrada inválida. Ingrese un número entero.")
 
+
 def confront():
     option = 0
     while option == 0:
         try:
             print("¿Con que IA te quieres enfrentar?")
             option = int(input("1)IA Random\n2)IA Defensiva\n3)IA Ganadora\n"))
-            if option == 1:
-                clean_screen()
-                return throw_random_machine
-            elif option == 2:
-                clean_screen()
-                return Block_Position
-            elif option == 3:
-                clean_screen()
-                return IA_Position
-            else:
-                clean_screen()
-                print("Por favor escoga una opción valida.")
-                option=0
+            return option
         except ValueError:
-            option=0
+            option = 0
             clean_screen()
             print("Entrada inválida. Ingrese un número entero")
